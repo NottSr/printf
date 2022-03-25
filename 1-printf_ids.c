c87fb2c9a00bd9f7894930db95731995edc241e8
NottSr <leonardo.br4@hotmail.com>
2022-03-24 20:27:30 -0500

    [alpha 1.9]: int debugging

--- 1-printf_ids.c
+++ 1-printf_ids.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
  * id_char - Our print function
@@ -37,15 +38,32 @@ int id_str(va_list inprif)
  */
 int id_int(va_list inprif)
 {
-	int din = va_arg(inprif, int);
-	int i = 0;
-
-	write(1, &din, 1);
-	while (din - 10 >= 0)
+	int i, num2, num3, zero = 0, ten = 10, count = 0;
+	int num = va_arg(inprif, int);
+	char nums;
+	num2 = num;
+	while (num2 != 0)
 	{
-		i++;
+		num2 /= 10;
+		count++;
 	}
-	return (i - 1);
+	for (i = 0; i <= count; i++)
+	{
+		num3 = num / ten;
+		num = num % ten;
+		if (num != 0)
+		{
+			zero = 1;
+		}
+		if (zero == 1)
+		{
+			nums = num3 + '0';
+			write(1, &nums, 1);
+		}
+	}
+	if (count < 0)
+		count++;
+	return (count);
 }
 /**
  * id_per - Our print function
--- main.c
+++ main.c
@@ -9,37 +9,39 @@
  */
 int main(void)
 {
+	/*char *str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";*/
 	int len;
 	int len2;
-	char *str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	unsigned int ui;
-	void *addr;
+	/*unsigned int ui;
+	void *addr;*/
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
+	/*ui = (unsigned int)INT_MAX + 1024;
+	addr = (void *)0x7ffe637541f0;*/
+	_printf("gggggg%i,\n", 5784);
+	printf("gggggg%i,\n", 5784);
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
-	_printf("Unsigned:[%u]\n", ui);
+	/*_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);*/
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
+	/*_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);*/
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	/*_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
 	printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
@@ -56,5 +58,21 @@ int main(void)
 	_printf(str);
 	_printf("%");
 	_printf("%!\n");
+	_printf("css%ccs%scscscs\n", 'T', "Test");
+	printf("css%ccs%scscscs\n", 'T', "Test");
+	_printf(str);
+	printf(str);
+	_printf("man gcc:\n%s", str);
+	printf("man gcc:\n%s", str);
+	_printf(NULL);
+	printf(NULL);
+	_printf("%c", '\0');
+	printf("%c", '\0');
+	_printf("%");
+	printf("%");
+	_printf("%!\n");
+	printf("%!\n");
+	_printf("%K\n");
+	printf("%K\n");*/
 	return (0);
 }
